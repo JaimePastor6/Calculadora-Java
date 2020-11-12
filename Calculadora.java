d606830e1c5efafc7dbb48ffd7e4ca2e8951f23e
Jaime Pastor <jaime.pastor6@educa.madrid.org>
2020-11-12 17:12:54 +0100

    Subimos archivo

---
+++ Calculadora.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un numero entero positivo:");
+        int num1 = sc.nextInt();
+        if (num1 < 0) {
+            System.out.println("El primer número introducido es negativo");
+            System.exit(1);
+        }
+        System.out.println("Introduzca un segundo numero entero: ");
+        int num2 = sc.nextInt();
+        if (num2<0) {
+            System.out.println("El segundo número introducido es negativo");
+            System.exit(1);
+        }
+        System.out.println("Introducir la operación deseada: ");
+
+        char ch = sc.next().charAt(0);
+
+        boolean b1 = '+' == ch;
+        boolean b2 = '-' == ch;
+        boolean b3 = '*' == ch;
+        boolean b4 = '/' == ch;
+        boolean b5 = !b1 && !b2 && !b3 && !b4;
+
+        if (b1) {
+            System.out.println("El resultado es "+ (num1 + num2));
+        } else if (b2) {
+            System.out.println("El resultado es "+ (num1 - num2));
+        }else if (b3) {
+            System.out.println("El resultado es " + (num1 * num2));
+        } else if (b4) {
+            System.out.println("El resultado es " + (num1 / num2));
+        }else if (b5) {
+            System.out.println("No es un caracter valido");
+        }
+    }
+}
